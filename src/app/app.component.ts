9c5b4d9fabcd2f296a9ac27ac64b1afc6338e9a8
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-09-30 10:58:47 -0600

    Update app.component.ts
    automatico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularClinica';
+
+
+export class AppComponent implements OnInit{
+ 
+    
+
+/*login:Login  = {
+  cuentaUsuario:null,
+  contrasenia: null
+};*/
+
+//para mostrar los datos de logins
+  //title = 'AngularClinica';
+  //API_ENDPOINT = 'http://127.0.0.1:8000/api';
+  //logins:Login[];
+//constructor(private loginService: LoginService,private httpClient: HttpClient){
+//httpClient.get(this.API_ENDPOINT +'/user').subscribe(  (data:Login[])=>{
+ // this.logins = data;}
+//);
+//}
+//fin de mostrar login
+ 
+constructor(){
+
+}
+
+ngOnInit() {
+    
+  }
 }
