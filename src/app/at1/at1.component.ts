632d71fc5f9bff707087a74eed40d4ccbf19162a
Marvingarci <eulalio17@yahoo.es>
2019-10-01 19:28:20 -0600

    Create at1.component.ts

---
+++ src/app/at1/at1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-at1',
+  templateUrl: './at1.component.html',
+  styleUrls: ['./at1.component.css']
+})
+export class At1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
