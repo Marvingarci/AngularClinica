51f36e19ae0bd3e9a4d287245a6566601a27b918
bagtrejo <braslytrejo@gmail.com>
2019-10-04 16:57:20 -0600

    Create antecedentes-obstetricos.ts

---
+++ src/app/interfaces/antecedentes-obstetricos.ts
@@ -0,0 +1,9 @@
+export interface AntecedentesObstetricos {
+    id_antecedente_obstetrico? : number;
+    fecha_termino_ult_embarazo? : string;
+    descripcion_termino_ult_embarazo? : string;
+    observaciones? : string;
+    id_paciente? : number;
+    created_at? : string;
+    updated_at? : string;
+}
