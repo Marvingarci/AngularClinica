8a8a99e4be8239d5a5b2e5bf867d9bbd45296082
sevillalberto <sevillajose.as@gmail.com>
2019-10-06 16:58:13 -0600

    Update dato-paciente.component.ts

--- src/app/dato-paciente/dato-paciente.component.ts
+++ src/app/dato-paciente/dato-paciente.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormularioService } from '../services/formulario.service';
+import { HttpClient } from '@angular/common/http';
+import { Paciente } from "../interfaces/paciente";
 
 @Component({
   selector: 'app-dato-paciente',
@@ -6,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dato-paciente.component.css']
 })
 export class DatoPacienteComponent implements OnInit {
-
-  constructor() { }
+  API_ENDPOINT = 'http://127.0.0.1:8000/api';
+  pacientes: Paciente[];
+  constructor(private formularioService: FormularioService, private httpClient: HttpClient) {
+    httpClient.get(this.API_ENDPOINT + '/formulario').subscribe((data: Paciente[]) =>{
+      this.pacientes = data;
+    });
+   }
 
   ngOnInit() {
   }
