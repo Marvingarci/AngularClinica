cf86ec4aeda34929accacb71260f939fb3c4acd8
Marvingarci <eulalio17@yahoo.es>
2019-10-06 17:36:51 -0600

    Update paciente.component.ts

--- src/app/paciente/paciente.component.ts
+++ src/app/paciente/paciente.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Paciente } from '../interfaces/paciente';
+import { HttpClient } from '@angular/common/http';
+import { FormularioService } from '../services/formulario.service';
+
 
 @Component({
   selector: 'app-paciente',
@@ -6,6 +10,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paciente.component.css']
 })
 export class PacienteComponent implements OnInit {
+
+  API_ENDPOINT = 'http://apiclinicaunah.test/api/';
+  paciente: Paciente[];
 
   constructor() { }
 
