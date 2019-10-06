555ec8a6ebe812db37b03b73ed76e7e0c64667a9
sevillalberto <sevillajose.as@gmail.com>
2019-10-06 16:59:08 -0600

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,42 +11,19 @@ import { Router } from '@angular/router';
 
 
 export class LoginComponent implements OnInit {
-<<<<<<< HEAD
 
 
   passwordType: string= 'password';
   passwordShown: boolean = false;
 
     
-=======
-    usuario: Usuario  = {
-    cuentaUsuario:null,
-    contrasenia: null
-  };
->>>>>>> 23fcc0400b31fa97a4276d070d8d3b7334910747
   constructor(private loginService: LoginService,private router: Router){
   }
   
   ngOnInit() {
   }
 
-<<<<<<< HEAD
    
   
-=======
-  saveLogin(){
-    this.router.navigate(['formulario']);
-  }
-  /*saveLogin(){this.loginService.save(this.usuario).subscribe( (data) =>{
-   console.log(data);
-    alert('pelicula guardada');   
-  },  (error)=>{
-    console.log(error);
-    alert('ocurrio un error');  
-  this.router.navigate(['formulario']);
-  });   
-  } */
-   
->>>>>>> 23fcc0400b31fa97a4276d070d8d3b7334910747
 
 }
