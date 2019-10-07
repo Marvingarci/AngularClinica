cc8abad4308267489b2f136bb90c1f0fbcdb27d7
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-10-06 19:12:23 -0600

    Create dato-paciente.component.ts

---
+++ src/app/dato-paciente/dato-paciente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dato-paciente',
+  templateUrl: './dato-paciente.component.html',
+  styleUrls: ['./dato-paciente.component.css']
+})
+export class DatoPacienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
