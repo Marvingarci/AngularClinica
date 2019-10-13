95d298f3aad96fa538e7e2c8eeffef617418bdba
bagtrejo <braslytrejo@gmail.com>
2019-10-12 19:27:02 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,10 +26,12 @@ import {MatStepperModule} from '@angular/material/stepper';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule, MatLabel} from '@angular/material/form-field'
+import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatNativeDateModule, MatOption } from '@angular/material/core';
+import { MatNativeDateModule} from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDividerModule} from '@angular/material/divider';
 
 const routes: Route[] = [
   {path: '', component: LoginComponent},
@@ -90,6 +92,8 @@ const routes: Route[] = [
     MatSelectModule,
     MatStepperModule,
     ReactiveFormsModule,
+    MatRadioModule,
+    MatDividerModule
     
   ],
   providers: [],
