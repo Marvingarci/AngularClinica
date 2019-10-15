7bb83af8fbfed447b625ffa21c0b21a78f952a15
bagtrejo <braslytrejo@gmail.com>
2019-10-14 21:30:38 -0600

    Update login.ts

--- src/app/interfaces/login.ts
+++ src/app/interfaces/login.ts
@@ -1,6 +1,12 @@
 export interface Login {
-    cuentalogin : string;
-    contrasenialogin : string;
+    id_login? : number;
+    cuenta : string;
+    clave : string;
+    nombre?: string;
+    carrera? : string;
+    centro? : string;
+    indice_global? : string;
+    indice_periodo? : string;  
     created_at? : string;
     updated_at? : string;
 }
