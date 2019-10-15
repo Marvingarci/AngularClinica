cbf4d637edc9bb587266da6c9019299bc28b3cf8
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-10-14 19:56:35 -0600

    Create login_admin.ts

---
+++ src/app/interfaces/login_admin.ts
@@ -0,0 +1,10 @@
+export interface LoginAdmin {
+    id?:number;
+    usuario_admin : string;
+    contrasenia_admin : string;
+    nombre_admin : string;
+    identidad_admin : string;
+    especialidad_admin : string;
+    created_at? : string;
+    updated_at? : string;
+}
