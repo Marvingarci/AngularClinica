8be0092a1619a10dccd454a1491725c4a05d801e
bagtrejo <braslytrejo@gmail.com>
2019-10-14 21:30:23 -0600

    Update login.service.ts

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Login } from '../interfaces/login';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  API_ENDPOINT = 'http://127.0.0.1:8000/api'
-  constructor(private httpClient :HttpClient) { }
+  API_ENDPOINT = 'http://127.0.0.1:8000/api/'
+  constructor(private httpClient :HttpClient) {}
+
+  guardarDatos(login: Login){
+    
+    const headers = new HttpHeaders({'Content-Type':'application/json'});
+    return this.httpClient.post(this.API_ENDPOINT + 'login', login, {headers: headers});
+    
+  
+  }
 
 }
