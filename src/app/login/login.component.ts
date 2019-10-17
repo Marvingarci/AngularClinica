20a2cfc277e55b8a05b601dc9e6ae3d102063536
Marvingarci <eulalio17@yahoo.es>
2019-10-16 21:51:28 -0600

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,9 @@ import { LoginService } from '../services/login.service';
 import { Router, RouterModule } from '@angular/router';
 import { Login } from '../interfaces/login';
 import { AppComponent } from "../app.component";
+import { Paciente } from "../interfaces/paciente";
+import { PacienteComponent } from '../paciente/paciente.component';
+import { FormularioService } from '../services/formulario.service';
 
 @Component({
   selector: 'app-login',
@@ -13,21 +16,25 @@ import { AppComponent } from "../app.component";
 
 
 export class LoginComponent implements OnInit {
-
-
-
   login: Login = {
     cuenta: null,
     clave: null
   };
 
+  pacientes: Paciente[];
+  Formulario: FormularioService;
+  pase: boolean=true;
 
-  
-
-    
-  constructor(private loginService: LoginService,private router: Router, activar: AppComponent){
+  constructor(private loginService: LoginService,private router: Router, private activar: AppComponent,  Formulario: FormularioService){
    activar.esconder();
-   
+
+
+   Formulario.get().subscribe((data: Paciente[])=>{
+    this.pacientes = data;
+    console.log(this.pacientes);
+  }, (error)=>{
+    console.log(error);
+  }); 
   }
 
 
@@ -38,14 +45,30 @@ export class LoginComponent implements OnInit {
 
 
   comprobarDatos(){
+
+    for (let index = 0; index < this.pacientes.length; index++) {
+      if (this.pacientes[index].numero_cuenta == this.login.cuenta) {
+        this.pase=false;
+      
+      }
+    }
+
+    if (this.pase == true) {
+      this.loginService.guardarDatos(this.login).subscribe( (data) =>{
+        console.log(data);   
+        alert('todo perron'); 
+         this.router.navigate(['/formulario']);
+      }, (error) => {
+        console.log(error);
+        alert('Clave incorrecta');
+      });
+      
+    }else{
+      this.router.navigate(['/datoPaciente']);
+    }
+
     
-    this.loginService.guardarDatos(this.login).subscribe( (data) =>{
-      console.log(data);   
-      alert('todo perron');  
-    }, (error) => {
-      console.log(error);
-      alert('se chorrio');
-    });
+    
 
   }
   
