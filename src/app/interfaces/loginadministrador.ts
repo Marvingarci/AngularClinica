52700908fd4c81eb51b387b65f03d6130c5ad0cd
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-10-26 21:20:46 -0600

    Create loginadministrador.ts

---
+++ src/app/interfaces/loginadministrador.ts
@@ -0,0 +1,7 @@
+export interface Loginadministrador {
+    id_loginadministrador? : number;
+    usuario : string;
+    contrasenia : string;  
+    created_at? : string;
+    updated_at? : string;
+}
