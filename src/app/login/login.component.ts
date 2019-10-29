9cc20178b3e71e9d5a07981d9e7e85612dc1c852
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-10-28 21:25:03 -0600

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,8 @@ import { Paciente } from "../interfaces/paciente";
 import { PacienteComponent } from '../paciente/paciente.component';
 import { FormularioService } from '../services/formulario.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { LoginAdmin } from '../interfaces/login_admin';
+import { LoginadminService } from '../services/loginadmin.service';
 
 @Component({
   selector: 'app-login',
@@ -47,28 +49,39 @@ export class LoginComponent implements OnInit {
     visitante: null,
     prosene: null,
   }
-  
-
+  scrap1: LoginAdmin = {  
+    id :null,
+    usuario_admin : null,
+    contrasenia_admin : null,
+    nombre_admin : null,
+    identidad_admin : null,
+    especialidad_admin : null,  
+  }
+//,Validators.pattern(/^[2][0-9]{10}$/)
   login_form = new FormGroup({
-    cuenta: new FormControl('',[Validators.required,Validators.pattern(/^[2][0-9]{10}$/)]),
+    cuenta: new FormControl('',[Validators.required]),
     clave: new FormControl('',[Validators.required]),
-
   });
-
- 
-
   login: Login = {
     cuenta: null,
     clave: null
   };
+  login_admin: LoginAdmin
+  login_admins: LoginAdmin[];
   paciente: Paciente;
   pacientes: Paciente[];
   Formulario: FormularioService;
   pase: boolean=true;
 
-  constructor(private loginService: LoginService,private router: Router, private activar: AppComponent,  Formulario: FormularioService){
+  constructor(private LoginAdminService: LoginadminService,private loginService: LoginService,private router: Router, private activar: AppComponent,  Formulario: FormularioService){
    activar.esconder();
 
+ this.LoginAdminService.getAdmin().subscribe((data: LoginAdmin[]) =>{
+    this.login_admins = data;
+    console.log(this.login_admins);
+  }, (error: any)=>{
+    console.log(error);
+  }); 
 
    Formulario.get().subscribe((data: Paciente[])=>{
     this.pacientes = data;
@@ -76,6 +89,8 @@ export class LoginComponent implements OnInit {
   }, (error)=>{
     console.log(error);
   }); 
+
+  
   }
 
 
@@ -89,6 +104,7 @@ export class LoginComponent implements OnInit {
     this.login.cuenta = this.login_form.get('cuenta').value;
     this.login.clave = this.login_form.get('clave').value;
 
+//PACIENTES
     for (let index = 0; index < this.pacientes.length; index++) {
       if (this.pacientes[index].numero_cuenta == this.login.cuenta) {
         this.pase=false;
@@ -115,10 +131,35 @@ export class LoginComponent implements OnInit {
         alert('Contraseña incorrecta')
       }      
     } 
+
+   //ADMIINISTRADORES
+    for (let indexx = 0; indexx < this.login_admins.length; indexx++) {
+      if (this.login_admins[indexx].usuario_admin == this.login.cuenta) {        
+        this.login_admin=this.login_admins[indexx];
+      }
+    }
+
+
+   if (this.login_admin.contrasenia_admin  == this.login.clave) {
+ console.log(this.login_admin.contrasenia_admin);console.log( this.login.cuenta);
+        this.router.navigate(['/principal/']);
+      }else{
+        
+ console.log(this.login_admin.contrasenia_admin);console.log( this.login.cuenta);
+        this.loading=false;
+        alert('Contraseña incorrecta')
+      } 
+
+
+
+
+
+
   }
 
+
+
   get cuenta(){return this.login_form.get('cuenta')};
-  get clave(){return this.login_form.get('clave')};
-  
+  get clave(){return this.login_form.get('clave')}; 
 
 }
