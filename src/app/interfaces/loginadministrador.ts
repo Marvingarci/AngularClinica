631adc369b82ca74658dd1a4fa09a2c95e97f07d
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-10-28 21:24:36 -0600

    Delete loginadministrador.ts

--- src/app/interfaces/loginadministrador.ts
+++
@@ -1,7 +0,0 @@
-export interface Loginadministrador {
-    id_loginadministrador? : number;
-    usuario : string;
-    contrasenia : string;  
-    created_at? : string;
-    updated_at? : string;
-}
