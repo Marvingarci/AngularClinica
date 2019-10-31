d641ffe6c2d9d80759af2975dcf1bdedc0f0e213
sevillalberto <sevillajose.as@gmail.com>
2019-10-31 10:07:42 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { ListadoVisitantesComponent } from './listado-visitantes/listado-visitan
 import { ListadoProseneComponent } from './listado-prosene/listado-prosene.component';
 import { VerPacienteComponent } from './ver-paciente/ver-paciente.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InventarioComponent } from './inventario/inventario.component';
+import { FormInventarioComponent } from './form-inventario/form-inventario.component';
 
 
 //Material Angular
@@ -43,8 +45,8 @@ import { MatTableModule } from '@angular/material'
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatMenuModule} from '@angular/material/menu';
-import { InventarioComponent } from './inventario/inventario.component';
 import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+
 
 
 
@@ -64,6 +66,7 @@ const routes: Route[] = [
   {path: 'datoPaciente', component: DatoPacienteComponent},
   {path: 'verPaciente', component: VerPacienteComponent},
   {path: 'inventario', component: InventarioComponent},
+  {path: 'formInventario', component: FormInventarioComponent},
 
 
   {path: 'loginadmin', component: LoginadminComponent},
@@ -107,6 +110,7 @@ const routes: Route[] = [
     VerPacienteComponent,
     DialogContentExampleDialog,
     InventarioComponent,
+    FormInventarioComponent,
     
 
     
