b5054eb5a7b56f80d6d03c9462adcc4bdb0fa43e
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-11-08 17:24:15 -0600

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,7 +22,6 @@ import { LoginadminService } from '../services/loginadmin.service';
 export class LoginComponent implements OnInit {
  hide = true;
  loading: boolean =false;
- color = 'background ';
   //input
   // scrap: Paciente = {
   //   id_paciente: null,
