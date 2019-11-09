3196c3e57bbc64c6800f2e97d704a77088bb8732
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-11-08 23:13:46 -0600

    Update loginadmin.component.ts

--- src/app/loginadmin/loginadmin.component.ts
+++ src/app/loginadmin/loginadmin.component.ts
@@ -12,9 +12,7 @@ import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
   styleUrls: ['./loginadmin.component.css']
 })
 export class LoginadminComponent implements OnInit {
-  hide = true;
-  //input
- 
+  hide = true; 
 
   loginadmin_form = new FormGroup({
     usuario: new FormControl('',[Validators.required,  Validators.minLength(6)]),    
@@ -30,8 +28,6 @@ export class LoginadminComponent implements OnInit {
     this.loginadmin_form.get('usuario').hasError('usuario') ? 'Not a valid usuario' :     '';
   }
 
-
-  
   login_admin:LoginAdmin = {    
     usuario_admin:null,
     contrasenia_admin:null,
@@ -59,8 +55,8 @@ export class LoginadminComponent implements OnInit {
   id:any;
   editing:boolean = false;
   admins: LoginAdmin[];
-  constructor(private activatedRoute:ActivatedRoute,private router: Router, 
-    activar: AppComponent, private login_adminservice:LoginadminService, private mensaje: MatSnackBar ) {
+  constructor(private activatedRoute:ActivatedRoute,private router: Router,activar: AppComponent,
+             private login_adminservice:LoginadminService, private mensaje: MatSnackBar ) {
     activar.esconder();
     this.getdato();
     this.id = this.activatedRoute.snapshot.params['id'];
@@ -68,29 +64,37 @@ export class LoginadminComponent implements OnInit {
     if(this.id){
       this.editing = true;
       this.login_adminservice.getAdmin().subscribe((data: LoginAdmin[]) =>{
-        this.admins = data;
-        this.login_admin = this.admins.find((m)=>{return m.id == this.id});
+      this.admins = data;
+      this.login_admin = this.admins.find((m)=>{return m.id == this.id});
 
         //establesco el valor al los formcontrol para que se visualizen en los respectivos inputs
-        this.usuario.setValue(this.login_admin.usuario_admin);
-            this.contrasenia.setValue(this.login_admin.contrasenia_admin);
-            this.contraseniaC.setValue(this.login_admin.contrasenia_admin);
-            this.nombre.setValue(this.login_admin.nombre_admin);
-            this.identidad.setValue(this.login_admin.identidad_admin);
-           this.especialidad.setValue(this.login_admin.especialidad_admin);
+      this.usuario.setValue(this.login_admin.usuario_admin);
+      this.contrasenia.setValue(this.login_admin.contrasenia_admin);
+      this.contraseniaC.setValue(this.login_admin.contrasenia_admin);
+      this.nombre.setValue(this.login_admin.nombre_admin);
+      this.identidad.setValue(this.login_admin.identidad_admin);
+      this.especialidad.setValue(this.login_admin.especialidad_admin);
 
-        console.log(this.login_admin.usuario_admin);
-        this.login_adminservice.idActualizar=this.login_admin.id;  
-       console.log(this.login_admin);      
+      console.log(this.login_admin.usuario_admin);
+      this.login_adminservice.idActualizar=this.login_admin.id;  
+      console.log(this.login_admin);      
       },(error)=>{
-        console.log(error);
+      console.log(error);
       });
 
-    }else{
+      }else{
       this.editing = false;
-    }
-    
-}//fin del constructor
+      }
+    }//fin del constructor
+
+getdato(){
+  this.login_adminservice.getAdmin().subscribe((data: LoginAdmin[]) =>{
+  this.admins = data;
+  },(error)=>{
+  console.log(error);
+  alert('Ocurrio un error');
+  });
+}
 
 showError(message: string) {
   const config = new MatSnackBarConfig();
@@ -101,56 +105,42 @@ showError(message: string) {
 
 
 
-   getdato(){
-    this.login_adminservice.getAdmin().subscribe((data: LoginAdmin[]) =>{
-      this.admins = data;
-    },(error)=>{
-      console.log(error);
-      alert('Ocurrio un error');
-    });
-  }
-
 ngOnInit() {
   this.getdato();
 }
 
   comprobarDatos(){
+  if(this.editing){  
+  if(this.loginadmin_form.valid){
 
+    this.login_admin.contrasenia_admin = this.loginadmin_form.get('contraseniaC').value; 
+    if(this.login_admin.contrasenia_admin ==this.loginadmin_form.get('contrasenia').value ){
+    this.login_admin.usuario_admin = this.usuario.value;
+    this.login_admin.contrasenia_admin = this.contraseniaC.value;
+    this.login_admin.nombre_admin = this.nombre.value;
+    this.login_admin.identidad_admin = this.identidad.value;
+    this.login_admin.especialidad_admin = this.especialidad.value;
 
-    if(this.editing){  
-   if(this.loginadmin_form.valid){
+    this.login_adminservice.put(this.login_admin).subscribe( (data) =>{
+    console.log(data);
+    this.router.navigate(['/principal/veradministradores']);
+    this.getdato();
+    this.showError('Administrador actualizado correctamente'); 
 
-                                      this.login_admin.contrasenia_admin = this.loginadmin_form.get('contraseniaC').value; 
-                                      if(this.login_admin.contrasenia_admin ==this.loginadmin_form.get('contrasenia').value ){
-                                      this.login_admin.usuario_admin = this.usuario.value;
-                                      this.login_admin.contrasenia_admin = this.contraseniaC.value;
-                                      this.login_admin.nombre_admin = this.nombre.value;
-                                      this.login_admin.identidad_admin = this.identidad.value;
-                                      this.login_admin.especialidad_admin = this.especialidad.value;
-                                      
-                                      this.login_adminservice.put(this.login_admin).subscribe( (data) =>{
-                                        console.log(data);
-                                        this.router.navigate(['/principal/veradministradores']);
-                                        this.getdato();
-                                        this.showError('Administrador actualizado correctamente'); 
-                                        
-                                      }, (error) => {
-                                        console.log(error);
-                                        alert('se chorrio');
-                                      });
-                                    }else{
-                                      this.showError('La contraseña no coincide');
-                                    }
-        }else{
-          this.showError('Ingrese los datos correctamente');
-        }
-
-     
-    
+    }, (error) => {
+    console.log(error);
+    alert('se chorrio');
+    });
+    }else{
+    this.showError('La contraseña no coincide');
+    }
+    }else{
+    this.showError('Ingrese los datos correctamente');
+    }
 
     }else{
-                                        this.login_admin.contrasenia_admin = this.loginadmin_form.get('contraseniaC').value; 
-                                        if(this.login_admin.contrasenia_admin ==this.loginadmin_form.get('contrasenia').value ){
+      this.login_admin.contrasenia_admin = this.loginadmin_form.get('contraseniaC').value; 
+      if(this.login_admin.contrasenia_admin ==this.loginadmin_form.get('contrasenia').value ){
       this.login_admin.usuario_admin = this.loginadmin_form.get('usuario').value;
       this.login_admin.contrasenia_admin = this.loginadmin_form.get('contraseniaC').value;
       this.login_admin.nombre_admin = this.loginadmin_form.get('nombre').value;
@@ -158,31 +148,29 @@ ngOnInit() {
       this.login_admin.especialidad_admin = this.loginadmin_form.get('especialidad').value;
 
       if(this.loginadmin_form.valid){
-        this.login_adminservice.saveloginadmin(this.login_admin).subscribe( (data) =>{
-            console.log(data);
-            this.getdato();
-            this.router.navigate(['/principal/veradministradores']);         
-            this.showError('Administrador creado con exito');
-          
-        }, (error) => {
-          console.log(error);
-          alert('se chorrio');
-        });
+      this.login_adminservice.saveloginadmin(this.login_admin).subscribe( (data) =>{
+      console.log(data);
+      this.getdato();
+      this.router.navigate(['/principal/veradministradores']);         
+      this.showError('Administrador creado con exito');
+      }, (error) => {
+      console.log(error);
+      alert('se chorrio');
+      });
       }else{
-        this.showError('Ingrese los datos correctamente');
-      }
-      
-    } 
-  else{
-    this.showError('La contraseña no coincide');
-    }}
-  }//fin del boton
+      this.showError('Ingrese los datos correctamente');
+      }      
+      } 
+      else{
+      this.showError('La contraseña no coincide');
+      }}
+      }//fin del boton
 
-  get usuario(){return this.loginadmin_form.get('usuario')};
-  get contrasenia(){return this.loginadmin_form.get('contrasenia')};
-  get contraseniaC(){return this.loginadmin_form.get('contraseniaC')};
-  get nombre(){return this.loginadmin_form.get('nombre')};
-  get identidad(){return this.loginadmin_form.get('identidad')};
-  get especialidad(){return this.loginadmin_form.get('especialidad')};
+      get usuario(){return this.loginadmin_form.get('usuario')};
+      get contrasenia(){return this.loginadmin_form.get('contrasenia')};
+      get contraseniaC(){return this.loginadmin_form.get('contraseniaC')};
+      get nombre(){return this.loginadmin_form.get('nombre')};
+      get identidad(){return this.loginadmin_form.get('identidad')};
+      get especialidad(){return this.loginadmin_form.get('especialidad')};
 
 }
