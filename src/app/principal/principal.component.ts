f076255078d260d1d71940e44c3fe9363a7194e3
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-11-09 22:43:37 -0600

    Update principal.component.ts

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AppComponent } from "../app.component";
+import {Location} from '@angular/common';
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
@@ -11,9 +12,13 @@ export class PrincipalComponent implements OnInit {
   typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
   
 
-  constructor(mostrar: AppComponent) {
+  constructor(mostrar: AppComponent,private _location: Location) {
     mostrar.mostrar();
    }
+
+    atras() {
+        this._location.back();
+    }
 
   ngOnInit() {
   }
