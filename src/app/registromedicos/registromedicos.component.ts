4d98578907e9f8cb771d285fd11e087841a42064
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-11-19 23:07:57 -0600

    Update registromedicos.component.ts

--- src/app/registromedicos/registromedicos.component.ts
+++ src/app/registromedicos/registromedicos.component.ts
@@ -12,6 +12,7 @@ import { MedicosService } from '../services/medicos.service';
   styleUrls: ['./registromedicos.component.css']
 })
 export class RegistromedicosComponent implements OnInit {
+  hide1 = false;
   hide = true; 
 
   medicos_form = new FormGroup({
@@ -109,6 +110,14 @@ export class RegistromedicosComponent implements OnInit {
     this.getdato();
   }
 
+  onKeydown(event1) {
+    if (event1.key === "Enter") {
+    this.hide1=true;
+    this.hide = true;
+  
+    }
+  }
+  
   comprobarDatos(){
     if(this.editing){  
     if(this.medicos_form.valid){
