53d6ae761bca81596b6ad6e7df5c5c327dcd8a6e
sevillalberto <sevillajose.as@gmail.com>
2019-11-23 14:43:55 -0600

    Update inventarios.service.ts

--- src/app/services/inventarios.service.ts
+++ src/app/services/inventarios.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inventario } from '../interfaces/inventario';
-import { Cita } from "../interfaces/Cita";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventariosService {
-  idCita: any;
   API_ENDPOINT = 'http://127.0.0.1:8000/api'
   getInventario(id_inventario: any){
     return this.httpClient.get(this.API_ENDPOINT+'/inventarios/'+id_inventario);
@@ -27,14 +25,4 @@ export class InventariosService {
     const headers = new HttpHeaders({'Content-Type':'application/json'});
     return this.httpClient.put(this.API_ENDPOINT+'/inventarios/' + inventario.id_inventario, inventario,{headers:headers});
   }
-
-  guardarCita(cita: Cita){
-    const headers = new HttpHeaders({'Content-Type':'application/json'});
-    return this.httpClient.post(this.API_ENDPOINT+'/citas',cita,{headers:headers});
-  }
-  
-  obtenerCita(id_paciente: any){
-    return this.httpClient.get(this.API_ENDPOINT+'/citas/'+id_paciente);
-  }
-
 }
