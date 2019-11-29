66a0c2b9e323562f66dda7141697f71cc62f0521
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2019-11-29 13:32:38 -0600

    Update principal.component.ts

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { AppComponent } from "../app.component";
 import {Location} from '@angular/common';
 import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { DialogContentExampleDialog } from '../dato-paciente/dato-paciente.component';
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
@@ -16,7 +18,7 @@ export class PrincipalComponent implements OnInit {
   public isSpinnerVisible:boolean; 
   // @Output() messageEvent = new EventEmitter<boolean>();  esto es para mandar string de un componente a otro
 
-  constructor(mostrar: AppComponent,private router: Router) {
+  constructor(mostrar: AppComponent,private router: Router,public dialog: MatDialog) {
     mostrar.mostrar();  
 }
 
@@ -24,11 +26,13 @@ abrirside() {
       this.opened != this.opened;         
     }
 
-    
-    
-   
-    ngOnInit() {
+    ayuda(){
+  const dialogRef = this.dialog.open(DialogContentExampleDialog1, {disableClose:false,panelClass: 'custom-dialog-container1'});
   }
+      
+      
+   
+    ngOnInit() {}
   
 }
 
@@ -48,3 +52,13 @@ abrirside() {
 
 
 
+@Component({
+  selector: 'dialog-content-example-dialog1',
+  templateUrl: 'dialog-content-example-dialog1.html',
+})
+
+export class DialogContentExampleDialog1 {
+ 
+  constructor( ){   
+   }
+}
