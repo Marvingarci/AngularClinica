84a17ebb64d11b3dfa88ff6500a71e8d24ce57e1
bagtrejo <braslytrejo@gmail.com>
2019-12-01 14:40:05 -0600

    Create paciente-hospitalaria-quirurgica.ts

---
+++ src/app/interfaces/paciente-hospitalaria-quirurgica.ts
@@ -0,0 +1,9 @@
+export interface PacienteHospitalariaQuirurgica {
+
+    id_paciente: number;
+    fecha: string;
+    tiempo_hospitalizacion: string;
+    diagnostico: string;
+    tratamiento: string;
+    
+}
