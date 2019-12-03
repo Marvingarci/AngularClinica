699ff9fbb5e123e8d5cfed8f34d0cf9ff118d25e
bagtrejo <braslytrejo@gmail.com>
2019-12-03 16:48:08 -0600

    Update formulario.component.ts

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -351,6 +351,8 @@ export class FormularioComponent implements OnInit, AfterViewInit {
       controlador.enable({ onlySelf: true });
     });
 
+    this.eliminarLabelStep(4);
+
   }
 
   borrarInputs(formControl: FormControl[]) {
@@ -602,15 +604,20 @@ export class FormularioComponent implements OnInit, AfterViewInit {
     console.log('step index seleccionado');
     console.log(event.selectedIndex);
     
+    this.eliminarLabelStep(event.selectedIndex);
+      
 
+
+/*
     if(event.selectedIndex == 0){
       
       this.step = 0;
 
       
-      this.element = document.getElementsByClassName('mat-step-label')[0];
-      this.element.innerHTML = "Datos Generales";
-      this.eliminarLabelStep([1,2,3,4,5,6,7]);
+      // this.element = document.getElementsByClassName('mat-step-label')[0];
+      // this.element.innerHTML = "Datos Generales";
+
+      this.eliminarLabelStep(0);
       
 
       // this.datosGenerales = 'Datos Generales';
@@ -636,10 +643,10 @@ export class FormularioComponent implements OnInit, AfterViewInit {
       // var element1: any =  document.getElementsByClassName('mat-step-label')[0];
       // element1.style.display = "none";
 
-      this.element = document.getElementsByClassName('mat-step-label')[1];
-      this.element.innerHTML = "Antecedentes Familiares";
+      //this.element = document.getElementsByClassName('mat-step-label')[1];
+      //this.element.innerHTML = "Antecedentes Familiares";
 
-      this.eliminarLabelStep([0,2,3,4,5,6,7]);
+      this.eliminarLabelStep(1);
 
       // this.datosGenerales = '';
       // this.antecedentesFamiliares = 'Antecedentes Familiares';
@@ -657,10 +664,10 @@ export class FormularioComponent implements OnInit, AfterViewInit {
       this.step = 2;
 
 
-      this.element = document.getElementsByClassName('mat-step-label')[2];
-      this.element.innerHTML = "Antecedentes Personales";
+      //this.element = document.getElementsByClassName('mat-step-label')[2];
+      //this.element.innerHTML = "Antecedentes Personales";
 
-      this.eliminarLabelStep([0,1,3,4,5,6,7]);
+      this.eliminarLabelStep(2);
 
 
       // this.datosGenerales = '';
@@ -681,7 +688,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
       // var element: any = document.getElementsByClassName('mat-step-label')[3];
       // element.innerHTML = "Actividad Sexual y Reproductiva";
 
-      // this.eliminarLabelStep([0,2,1,4,5,6,7]);
+      this.eliminarLabelStep(3);
 
       // this.datosGenerales = '';
       // this.antecedentesFamiliares = '';
@@ -704,7 +711,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
         this.element = document.getElementsByClassName('mat-step-label')[4];
         this.element.innerHTML = "Hábitos Toxicológicos Personales";
 
-        this.eliminarLabelStep([0,2,3,1,5,6,7]);
+        this.eliminarLabelStep(4);
 
         // this.datosGenerales = '';
         // this.antecedentesFamiliares = '';
@@ -722,7 +729,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
         this.element = document.getElementsByClassName('mat-step-label')[4];
         this.element.innerHTML = "Antecedentes Ginecológicos";
 
-        this.eliminarLabelStep([0,2,3,1,5,6,7]);
+        this.eliminarLabelStep(4);
 
 
         // this.datosGenerales = '';
@@ -741,7 +748,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
         this.element = document.getElementsByClassName('mat-step-label')[4];
         this.element.innerHTML = "Planificación Familiar";
 
-        this.eliminarLabelStep([0,2,3,1,5,6,7]);
+        this.eliminarLabelStep(4);
 
 
         // this.datosGenerales = '';
@@ -759,7 +766,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
         this.element = document.getElementsByClassName('mat-step-label')[4];
         this.element.innerHTML = "Antecedentes Obstétricos";
 
-        this.eliminarLabelStep([0,2,3,1,5,6,7]);
+        this.eliminarLabelStep(4);
 
         // this.datosGenerales = '';
         // this.antecedentesFamiliares = '';
@@ -778,7 +785,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
         this.element = document.getElementsByClassName('mat-step-label')[5];
         this.element.innerHTML = "Hábitos Toxicológicos Personales";
 
-        this.eliminarLabelStep([0,2,6,1,4,7,3]);
+        this.eliminarLabelStep(5);
 
         // this.datosGenerales = '';
         // this.antecedentesFamiliares = '';
@@ -794,7 +801,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
         this.element = document.getElementsByClassName('mat-step-label')[5];
         this.element.innerHTML = "Hábitos Toxicológicos Personales";
 
-        this.eliminarLabelStep([0,2,6,1,4,7,3]);
+        this.eliminarLabelStep(5);
 
         // this.datosGenerales = '';
         // this.antecedentesFamiliares = '';
@@ -810,7 +817,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
         this.element = document.getElementsByClassName('mat-step-label')[5];
         this.element.innerHTML = "Planificación Familiar";
 
-        this.eliminarLabelStep([0,2,6,1,4,7,3]);
+        this.eliminarLabelStep(5);
 
         // this.datosGenerales = '';
         // this.antecedentesFamiliares = '';
@@ -830,7 +837,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
         this.element = document.getElementsByClassName('mat-step-label')[6];
         this.element.innerHTML = "Antecedentes Ginecológicos";
 
-        this.eliminarLabelStep([0,2,5,1,4,3,7]);
+        this.eliminarLabelStep(6);
 
         // this.datosGenerales = '';
         // this.antecedentesFamiliares = '';
@@ -850,7 +857,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
         this.element = document.getElementsByClassName('mat-step-label')[6];
         this.element.innerHTML = "Hábitos Toxicológicos Personales";
 
-        this.eliminarLabelStep([0,2,5,1,4,3,6]);
+        this.eliminarLabelStep(7);
 
         // this.datosGenerales = '';
         // this.antecedentesFamiliares = '';
@@ -865,18 +872,35 @@ export class FormularioComponent implements OnInit, AfterViewInit {
         
       }
     }
-    
+*/
+
   }
 
-  eliminarLabelStep(index: any[]){
+  eliminarLabelStep(index: number){
+    this.step = index;
+    let elementos: any = document.getElementsByClassName('mat-step-label');
 
+    for (let i = 0; i < elementos.length; i++) {
+      const element: any= elementos[i];
+      element.style.display= "none";
+      console.log(element);
+    }
+    console.log(index);
+
+    elementos[index].style.display = "";
+
+/*
     index.forEach(i=> {
 
-      this.element = document.getElementsByClassName('mat-step-label')[i];
-      this.element.style.display= "none";
+      let element: any  = document.getElementsByClassName('mat-step-label')[i];
+      element.style.display= "none";
+
+
+      console.log(element);
+      console.log(i);
 
       
-    });
+    });*/
 
   }
 
@@ -1191,7 +1215,7 @@ export class FormularioComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void{  
 
-    // this.eliminarLabelStep([1,2,3,4]);
+    this.eliminarLabelStep(0);
 
     // console.log('step: '+this.step);
 
