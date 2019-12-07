cb4e443c51f42168900d8070fd374da9ebae2669
bagtrejo <braslytrejo@gmail.com>
2019-12-07 12:42:35 -0600

    Update medicos.component.ts

--- src/app/medicos/medicos.component.ts
+++ src/app/medicos/medicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject, AfterViewInit, Input, OnChanges, OnDestroy, AfterContentInit } from '@angular/core';
 import { Medicos } from '../interfaces/medicos';
 import { HttpClient } from '@angular/common/http';
 import {MatTableDataSource} from '@angular/material/table';
@@ -8,6 +8,10 @@ import { LoginadminService } from '../services/loginadmin.service';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatSnackBar, MatSnackBarConfig } from '@angular/material';
 
 import { MedicosService } from '../services/medicos.service';
+import { FormularioService } from '../services/formulario.service';
+import { get } from 'http';
+import { ThrowStmt } from '@angular/compiler';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 export interface Medicos {
   id?: string;
@@ -21,13 +25,19 @@ export interface Medicos {
 @Component({
   selector: 'app-medicos',
   templateUrl: './medicos.component.html',
-  styleUrls: ['./medicos.component.css']
+  styleUrls: ['./medicos.component.css'],
+  providers: []
 })
-export class MedicosComponent implements OnInit {
+
+export class MedicosComponent implements OnInit{
+  
+
   API_ENDPOINT = 'http://127.0.0.1:8000/api';
  
   medicos: Medicos[];
   dataSource: any;
+
+
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   medico:Medicos = {   
@@ -43,14 +53,13 @@ export class MedicosComponent implements OnInit {
 
   constructor(private medicoService:MedicosService,private activatedRoute: ActivatedRoute,public dialog: MatDialog,
   private LoginAdminService: LoginadminService,private httpClient: HttpClient, private router:Router) { 
-    this.getMedico();    
+
     this.id = this.activatedRoute.snapshot.params['id'];
 
     if(this.id){
-      this.medicoService.getMedico().subscribe((data: Medicos[]) =>{
+      this.medicoService.obtenerMedicos().subscribe((data: Medicos[]) =>{
         this.meds = data;
         this.medico = this.meds.find((m)=>{return m.id == this.id});
-        console.log(this.medico.usuarioM);
         this.medicoService.idActualizar=this.medico.id;  
        console.log(this.medico);      
       },(error)=>{
@@ -61,11 +70,10 @@ export class MedicosComponent implements OnInit {
   }
 
   getMedico(){
-    this.medicoService.getMedico().subscribe((data: Medicos[]) =>{
-      this.meds=data;
-      console.log(this.meds);
-      this.meds = this.meds.filter(paciente => paciente);
-      this.dataSource =  new MatTableDataSource(this.meds);
+    this.medicoService.obtenerMedicos().subscribe((data: Medicos[]) =>{
+      this.medicos=data;
+      this.medicos = this.medicos.filter(paciente => paciente);
+      this.dataSource = new MatTableDataSource(this.medicos);
     },(error)=>{
       console.log(error);
       alert('Ocurrio un error');
@@ -81,114 +89,93 @@ export class MedicosComponent implements OnInit {
   }  
 
   ngOnInit() {
-    this.getMedico(); 
+    this.getMedico();
+  } 
+
+  abrirDialogo(id){
+
+    const dialogRef = this.dialog.open(DialogoMedico, {
+      disableClose:true, 
+      panelClass: 'borrar',
+      data: id
+    });  
+
+    dialogRef.afterClosed().subscribe(result => {
+
+      this.getMedico();
+ 
+    });
+    
   }
 
-  borrarmedico(){
-    const dialogRef = this.dialog.open(Borrarmedico, {disableClose:true,panelClass: 'borrar'});  
-  }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 @Component({
-  selector: 'dialog-borrar-medico',
+  selector: 'dialogoMedico',
   templateUrl: 'dialog-borrar-medico.html',
+  
 })
+
  
-export class Borrarmedico {
+export class DialogoMedico implements OnDestroy{
+
+  constructor(public dialogRef: MatDialogRef<DialogoMedico>,
+    private medicoService:MedicosService,
+    private mensaje: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data: any) {}
+
   
-  medico:Medicos = {   
-    usuarioM:null,
-    contraseniaM:null,
-    nombreM:null,
-    identidadM:null,
-    especialidadM:null
-  };
+  salir(): void {
+    this.dialogRef.close();
+  }
 
-  id: any;
-  meds: Medicos[];
-
-
-constructor(public dialog: MatDialog,private router: Router,private mensaje: MatSnackBar,private medicoService:MedicosService,
-          private activatedRoute: ActivatedRoute) {    
-this.dialog.closeAll;   
-this.getPacientes();
-}
-
- showError(message: string) {
-  const config = new MatSnackBarConfig();
-  config.panelClass = ['background-red'];
-  config.duration = 2000;
-  this.mensaje.open(message, null, config);
-}
-
-getPacientes(){
-  this.medicoService.getMedico().subscribe((data: Medicos[]) =>{
-    this.meds=data;
-    console.log(this.meds);
-  },(error)=>{
-    console.log(error);
-    alert('Ocurrio un error');
-  });
-}
-
-
-ngOnInit() {
-  this.getPacientes();
-}
-
-  salir(){
-
-    this.medico.id  = this.medicoService.idActualizar;
-  
-    if(this.medico.id){
-    this.medicoService.getMedico().subscribe((data: Medicos[]) =>{
-      this.meds = data;
-      this.medico = this.meds.find((m)=>{return m.id == this.medico.id});
-      //console.log(this.login_admin.usuario_admin);
-      // this.login_adminservice.idActualizar=this.login_admin.id;  
-     console.log(this.medico.id);      
-    },(error)=>{
-      console.log(error);
+  Borrarmedico(){
+    this.medicoService.borrarMedico(this.data).subscribe((data)=>{
+      this.showError('Administrador eliminado correctamente');
     });
   }
-  console.log(this.medico.id);
 
-
-
-
-     this.medicoService.delete(this.medico.id).subscribe((data)=>{       
-    this.showError('Administrador eliminado correctamente'); 
-    console.log(data);
-    
-  this.router.navigate(['/principal/medicos']);
-    
-   
-},(error)=>{console.log(error);
-  });
+  showError(message: string) {
+    const config = new MatSnackBarConfig();
+    config.panelClass = ['background-red'];
+    config.duration = 2000;
+    this.mensaje.open(message, null, config);
   }
+
+  ngOnDestroy(): void {
+    this.medicoService.obtenerMedicos().subscribe();
+  }
+
+  // RecargarMedicos(){
+  //   let index: number;
+
+  //   this.medicoService.obtenerMedicos().subscribe((data: Medicos[])=>{
+  //     medicos = data;
+  //     dataSource = new MatTableDataSource(medicos);
+
+  //     // this.datasourcePadre = new MatTableDataSource(this.medicos);
+  //     // this.datosDesdeElPadre.estollegadelpadre = this.datasourcePadre;
+
+  //     // this.medicos = data;
+  //     // this.medico = this.medicos.find((m)=>{return m.id == this.data.id});
+  //     // index = this.medicos.indexOf(this.medico);
+  //     // this.medicos.splice(index, 1);
+  //     // this.dataSource = new MatTableDataSource(this.medicos);
+
+  //     // console.log('medico');
+  //     // console.log(this.medicosComponent.medico);
+  //     // console.log('index');
+  //     // console.log(index = this.medicosComponent.medicos.indexOf(this.medicosComponent.medico));
+
+  //   });
+    
+  // }
+
+ 
+  
+
 }
