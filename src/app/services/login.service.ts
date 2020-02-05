8fef900b3cad2e2fbc5a3272012f6ce35242cbbe
bagtrejo <braslytrejo@gmail.com>
2020-02-04 20:28:39 -0600

    Update login.service.ts

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -9,13 +9,16 @@ import { FormularioService } from "../services/formulario.service";
 export class LoginService {
   porMientras: string;//variable donde se guarda la contrase;a por si el usuario no registra la nueva
 
-  API_ENDPOINT = 'http://127.0.0.1:8000/api/'
+  API_ENDPOINT = 'http://127.0.0.1:8000/api/';
+  
   constructor(private httpClient :HttpClient, formularioService: FormularioService) {}
-  idpaciente:any;//este es para recuperar el id y redireccionar 
+  idpaciente:any; //este es para recuperar el id y redireccionar 
 
   guardarDatos(login: Login){
+
     const headers = new HttpHeaders({'Content-Type':'application/json'});
     return this.httpClient.post(this.API_ENDPOINT + 'login', login, {headers: headers});
+
   }
 
   
