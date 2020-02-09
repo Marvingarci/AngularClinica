ea22a87c7854811c3a7308faa2f933642f067327
bagtrejo <braslytrejo@gmail.com>
2020-02-08 21:12:51 -0600

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,7 @@ import { LoginAdmin } from '../interfaces/login_admin';
 import { LoginadminService } from '../services/loginadmin.service';
 import { Medicos } from '../interfaces/medicos';
 import { MedicosService } from '../services/medicos.service';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-login',
@@ -49,11 +50,10 @@ export class LoginComponent implements OnInit {
   medico: Medicos;
   medicos: Medicos[];
 
-  Formulario: FormularioService;
   pase: boolean = true;
 
-  constructor(private LoginAdminService: LoginadminService, private loginService: LoginService, private medicosService: MedicosService, private router: Router,
-    private activar: AppComponent, Formulario: FormularioService, private mensaje: MatSnackBar) {
+  constructor(private LoginAdminService: LoginadminService, private loginService: LoginService, private medicosService: MedicosService,
+    private router: Router, private activar: AppComponent, private formularioService: FormularioService, private mensaje: MatSnackBar) {
     activar.esconder();
 
     this.LoginAdminService.getAdmin().subscribe((data: LoginAdmin[]) => {
@@ -70,12 +70,12 @@ export class LoginComponent implements OnInit {
       console.log(error);
     });
 
-    Formulario.obtenerPacientes().subscribe((data: Paciente[]) => {
-      this.pacientes = data;
-      console.log(this.pacientes);
-    }, (error) => {
-      console.log(error);
-    });
+    // formularioService.obtenerPacientes().subscribe((data: Paciente[]) => {
+    //   this.pacientes = data;
+    //   console.log(this.pacientes);
+    // }, (error) => {
+    //   console.log(error);
+    // });
   }
 
   showError(message: string) {
@@ -87,102 +87,194 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() { }
 
-//FUNCION QUE HACE TODO EL MACANEO
-continuar(){
-  this.hide = false;
-  this.loading = true;
-  this.login.cuenta = this.cuenta.value;
-  this.login.password = this.clave.value;
-  this.loginService.porMientras = this.login_form.get('clave').value;
+  //FUNCION QUE HACE TODO EL MACANEO
+  continuar() {
 
-  //pacientes
-  for (let index = 0; index < this.pacientes.length; index++) {
-    if (this.pacientes[index].numero_cuenta == this.login.cuenta || this.pacientes[index].numero_identidad == this.login.cuenta) {
-      this.pase = false;
-      this.paciente = this.pacientes[index];
-    }
-  }
-  if (this.pase == true) {
-    // si el usuario es nuevo entonces lo registro.
-    this.loginService.guardarDatos(this.login).subscribe((data: any) => {
-      //guardo el token en el localstorage para poder obtenerlo despues.
-      localStorage.setItem("token", data.token);
-      this.showError('Llene el siguiente formulario');
-      this.router.navigate(['/formulario']);
-    }, (error) => {
-      this.loading = false;
-      console.log(error);
-      this.showError('Cuenta incorrecta');
+    this.hide = false;
+    this.loading = true;
+    this.loginService.porMientras = this.clave.value;
+
+    //pacientes
+
+    //verifico en la base de datos si el usuario fue logueado ya ó es primera vez.
+    this.loginService.obtenerUsuario(this.cuenta.value, this.clave.value).subscribe((data: any) => {
+
+      //si el resultado arroja un valor diferente de null, entonces el usuario existe en la base de datos.
+      if (data != null) {
+
+        //si el resultado arroja un valor difente a contrasenia incorrecta entonces el usuario esta verificado y
+        //puede loguearse correctamente
+        if (data != 'contrasenia incorrecta') {
+
+
+          //establezco a la interfaz login el atributo cuenta y password 
+          //los valores que el usuario ingresa una vez que se sabe que son veridicos.
+          this.login.cuenta = this.cuenta.value;
+          this.login.password = this.clave.value;
+
+          // si el usuario ya se encuentra registrado entonces solo se logueára.
+          this.loginService.loguear(this.login).subscribe((data: any) => {
+            //guardo el token en el localstorage para poder obtenerlo despues.
+            localStorage.setItem("token", data.token);
+
+            //recupero al paciente introduciendo su numero de cuenta para poder recuperar su id
+            // y redireccionarlo a su respectivo informacion.
+            this.formularioService.obtenerPacientePorCuenta(this.cuenta.value).subscribe((data: Paciente) => {
+              this.paciente = data;
+
+              this.router.navigate(['/datoPaciente/' + this.paciente.id_paciente]);
+              this.showError('Bienvenido');
+            });
+
+
+          });
+
+
+        // si el resultado arroja que la contrasenia es incorrecta el usuario existe en la base
+        // de datos pero su contrasenia no es correcta.
+        } else {
+          // console.log(this.medico.contraseniaM); console.log(this.login.cuenta);
+          this.loading = false;
+          this.showError('Contraseña Incorrecta');
+        }
+
+
+        // si el resultado arroja null entonces el usuario no existe en la base de datos
+        // por lo que es nuevo y tiene que hacer su registro correspondiente.
+      } else {
+
+        this.login.cuenta = this.cuenta.value;
+        this.login.password = this.clave.value;
+
+        // si el usuario es nuevo entonces lo registro.
+        this.loginService.guardarDatos(this.login).subscribe((data: any) => {
+
+          //guardo el token en el localstorage para poder obtenerlo despues.
+          localStorage.setItem("token", data.token);
+
+          this.showError('Llene el siguiente formulario');
+          this.router.navigate(['/formulario']);
+
+        }, (error) => {
+          
+          this.loading = false;
+          console.log(error);
+          this.showError('Cuenta incorrecta');
+        });
+
+      }
+
     });
-  } else {
 
-    if (this.paciente.contrasenia == this.login.password) {
+    // for (let index = 0; index < this.pacientes.length; index++) {
+    //   if (this.pacientes[index].numero_cuenta == this.login.cuenta || this.pacientes[index].numero_identidad == this.login.cuenta) {
+    //     this.pase = false;
+    //     this.paciente = this.pacientes[index];
+    //   }
+    // }
 
-      this.login.cuenta = this.cuenta.value;
-      this.login.password = this.clave.value;
+    // if (this.pase == true) {
 
-      // si el usuario ya se encuentra registrado entonces solo se logueára.
-      this.loginService.loguear(this.login).subscribe((data: any) => {
-      //guardo el token en el localstorage para poder obtenerlo despues.
-        localStorage.setItem("token", data.token);
+    //   this.login.cuenta = this.cuenta.value;
+    //   this.login.password = this.clave.value;
 
-        this.router.navigate(['/datoPaciente/' + this.paciente.id_paciente]);
-        this.showError('Bienvenido');
-      })
+    //   // si el usuario es nuevo entonces lo registro.
+    //   this.loginService.guardarDatos(this.login).subscribe((data: any) => {
+    //     //guardo el token en el localstorage para poder obtenerlo despues.
+    //     localStorage.setItem("token", data.token);
+    //     this.showError('Llene el siguiente formulario');
+    //     this.router.navigate(['/formulario']);
+    //   }, (error) => {
+    //     this.loading = false;
+    //     console.log(error);
+    //     this.showError('Cuenta incorrecta');
+    //   });
 
-    } else {
-      this.loading = false;
-      this.showError('Contraseña Incorrecta');
+    // } else {
+
+    // this.loginService.encryptarPassword(this.clave.value).subscribe((data)=>{
+    //   this.clave.setValue(data);
+
+    //   console.log('esta es la clave: \n'+data);
+
+    //   if(this.login.password == data){
+    //     console.log('true');
+    //   }else{
+    //     console.log('false');
+    //   }
+
+    // });
+
+    //   console.log('esta es el password: \n'+ this.login.password);
+
+    //   // this.conversionDecryptOutput = CryptoJS.AES.decrypt(this.encryptText.trim(), this.decPassword.trim()).toString(CryptoJS.enc.Utf8);
+
+
+    //   if (this.login.password == this.clave.value) {
+
+    //     // si el usuario ya se encuentra registrado entonces solo se logueára.
+    //     this.loginService.loguear(this.login).subscribe((data: any) => {
+    //       //guardo el token en el localstorage para poder obtenerlo despues.
+    //       localStorage.setItem("token", data.token);
+
+    //       this.router.navigate(['/datoPaciente/' + this.paciente.id_paciente]);
+    //       this.showError('Bienvenido');
+    //     })
+
+    //   } else {
+    //     this.loading = false;
+    //     this.showError('Contraseña Incorrecta');
+    //   }
+    // }
+
+    //medicos
+    for (let indexx = 0; indexx < this.medicos.length; indexx++) {
+      if (this.medicos[indexx].usuarioM == this.login.cuenta) {
+        this.medico = this.medicos[indexx];
+        if (this.medico.contraseniaM == this.login.password) {
+          console.log(this.medico.contraseniaM); console.log(this.login.cuenta);
+          this.showError('Bienvenido Medico');
+          this.router.navigate(['/principal/principal1']);
+        } else {
+          console.log(this.medico.contraseniaM); console.log(this.login.cuenta);
+          this.loading = false;
+          this.showError('Contraseña Incorrecta');
+        }
+      }
     }
-  }
 
-  //medicos
-  for (let indexx = 0; indexx < this.medicos.length; indexx++) {
-    if (this.medicos[indexx].usuarioM == this.login.cuenta) {
-      this.medico = this.medicos[indexx];
-      if (this.medico.contraseniaM == this.login.password) {
-        console.log(this.medico.contraseniaM); console.log(this.login.cuenta);
-        this.showError('Bienvenido Medico');
-        this.router.navigate(['/principal/principal1']);
-      } else {
-        console.log(this.medico.contraseniaM); console.log(this.login.cuenta);
-        this.loading = false;
-        this.showError('Contraseña Incorrecta');
+    //administradores
+    for (let indexx = 0; indexx < this.login_admins.length; indexx++) {
+      if (this.login_admins[indexx].usuario_admin == this.login.cuenta) {
+        this.login_admin = this.login_admins[indexx];
+        if (this.login_admin.contrasenia_admin == this.login.password) {
+          console.log(this.login_admin.contrasenia_admin); console.log(this.login.cuenta);
+          this.showError('Bienvenido Administrador');
+          this.router.navigate(['/principal/principal1']);
+        } else {
+          console.log(this.login_admin.contrasenia_admin); console.log(this.login.cuenta);
+          this.loading = false;
+          this.showError('Contraseña Incorrecta');
+        }
       }
     }
   }
 
-  //administradores
-  for (let indexx = 0; indexx < this.login_admins.length; indexx++) {
-    if (this.login_admins[indexx].usuario_admin == this.login.cuenta) {
-      this.login_admin = this.login_admins[indexx];
-      if (this.login_admin.contrasenia_admin == this.login.password) {
-        console.log(this.login_admin.contrasenia_admin); console.log(this.login.cuenta);
-        this.showError('Bienvenido Administrador');
-        this.router.navigate(['/principal/principal1']);
-      } else {
-        console.log(this.login_admin.contrasenia_admin); console.log(this.login.cuenta);
-        this.loading = false;
-        this.showError('Contraseña Incorrecta');
-      }
-    }
-  }
-}
 
-
-//EVENTO CUANDO SE DA ENTER
-   onKeydown1(event) {
+  //EVENTO CUANDO SE DA ENTER
+  onKeydown1(event) {
     if (event.key === "Enter") {
       this.hide = false;
     }
   }
+
   onKeydown(event) {
     if (event.key === "Enter") {
-     this.continuar();
+      this.continuar();
     }
   }
 
-//EVENTO CUANDO SE DA EN EL BOTON
+  //EVENTO CUANDO SE DA EN EL BOTON
   comprobarDatos() {
     this.continuar();
   }
