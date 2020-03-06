c9b7d62b2fc099ad3a809e0cf165421cdd7db3a4
bagtrejo <braslytrejo@gmail.com>
2020-03-06 09:12:26 -0600

    Update loginadmin.component.ts

--- src/app/loginadmin/loginadmin.component.ts
+++ src/app/loginadmin/loginadmin.component.ts
@@ -9,6 +9,7 @@ import { Administrador } from '../interfaces/administrador';
 import { LoginService } from '../services/login.service';
 import { Login } from '../interfaces/login';
 import { ThemeService } from 'ng2-charts';
+import { DialogoVerificarPermisoComponent } from '../dialogo-verificar-permiso/dialogo-verificar-permiso.component';
 
 
 export interface select {
@@ -23,8 +24,8 @@ export interface select {
 })
 export class LoginadminComponent implements OnInit {
 
-  esconderClave: boolean ;
-  esconderConfirmacionClave : boolean;
+  esconderClave: boolean;
+  esconderConfirmacionClave: boolean;
 
   loginadmin_form = new FormGroup({
 
@@ -54,19 +55,21 @@ export class LoginadminComponent implements OnInit {
   };
 
   id: any;
-  editing: boolean = false;
+  editando: boolean = false;
   admins: Administrador[];
   constructor(private activatedRoute: ActivatedRoute, private router: Router, activar: AppComponent,
     private login_adminservice: LoginadminService, private mensaje: MatSnackBar,
     public dialogo: MatDialog,
     private usuarioAdminUnicoService: UsuarioAdminUnicoService) {
-    activar.esconder();
-    this.getdato();
+
     this.id = this.activatedRoute.snapshot.params['id'];
+
+    activar.esconder();
+    this.getAdministradores();
 
     if (this.id) {
 
-      this.editing = true;
+      this.editando = true;
 
       this.login_adminservice.obtenerAdministrador(this.id).subscribe((data: any) => {
 
@@ -87,12 +90,12 @@ export class LoginadminComponent implements OnInit {
 
     } else {
 
-      this.editing = false;
+      this.editando = false;
 
     }
   }//fin del constructor
 
-  getdato() {
+  getAdministradores() {
 
     this.login_adminservice.obtenerAdministradores().subscribe((data: Administrador[]) => {
 
@@ -116,7 +119,7 @@ export class LoginadminComponent implements OnInit {
 
 
   ngOnInit() {
-    this.getdato();
+    this.getAdministradores();
   }
 
 
@@ -131,18 +134,89 @@ export class LoginadminComponent implements OnInit {
   }
 
 
-  llamarDialogo(){
+  llamarDialogo() {
 
     if (this.loginadmin_form.valid) {
 
 
       if (this.contraseniaC.value == this.contrasenia.value) {
 
-        const dialogRef = this.dialogo.open(DialogoVerificar, {
+        const dialogRef = this.dialogo.open(DialogoVerificarPermisoComponent, {
           disableClose: true,
           panelClass: 'verificar',
-          data: { id: this.id, editando: this.editing, formulario: this.loginadmin_form }
+          data: { id: this.id, editando: this.editando, formulario: this.loginadmin_form}
 
+        });
+
+        dialogRef.afterClosed().subscribe(confirmacion => {
+
+          if (confirmacion) {
+
+            if (this.editando) {
+              // this.disabledloginadmin = true;
+
+              // this.administrador.password = this.data.formulario.get('contraseniaC').value;
+              this.administrador.id_administrador = this.id;
+              this.administrador.usuario = this.usuario.value;
+              // this.administrador.password = this.contraseniaC.value;
+              this.administrador.nombre_completo = this.nombre.value;
+              this.administrador.identidad = this.identidad.value;
+
+              this.login_adminservice.actualizarAdministrador(this.administrador).subscribe((data) => {
+
+                this.router.navigate(['/principal/veradministradores']);
+                this.getAdministradores();
+                this.showError('Administrador actualizado correctamente');
+
+              }, (error) => {
+                console.log(error);
+                alert('se chorrio');
+              });
+
+
+
+            } else {
+
+              this.administrador.password = this.contraseniaC.value;
+
+              if (this.administrador.password == this.contrasenia.value) {
+
+                this.administrador.usuario = this.usuario.value;
+                this.administrador.password = this.contrasenia.value;
+                this.administrador.nombre_completo = this.nombre.value;
+                this.administrador.identidad = this.identidad.value;
+
+                if (this.loginadmin_form.valid) {
+
+                  // this.disabledloginadmin = true;
+
+                  this.login_adminservice.guardarAdministrador(this.administrador).subscribe((data) => {
+
+                    this.getAdministradores();
+
+                    this.router.navigate(['/principal/veradministradores']);
+
+                    this.showError('Administrador creado con exito');
+
+                  }, (error) => {
+
+                    console.log(error);
+
+                  });
+
+                } else {
+
+                  this.showError('Ingrese los datos correctamente');
+                }
+
+              } else {
+
+                this.showError('La contraseña no coincide');
+
+              }
+            }
+
+          }
         });
 
       } else {
@@ -155,10 +229,11 @@ export class LoginadminComponent implements OnInit {
     }
 
   }
+
   comprobarDatos() {
 
     this.llamarDialogo();
-    
+
   }//fin del boton
 
   get usuario() { return this.loginadmin_form.get('usuario') };
@@ -171,182 +246,182 @@ export class LoginadminComponent implements OnInit {
 
 
 //dialogo
-@Component({
-  selector: 'dialogoVerificar',
-  templateUrl: 'dialogoVerificar.html',
-})
+// @Component({
+//   selector: 'dialogoVerificar',
+//   templateUrl: 'dialogoVerificar.html',
+// })
 
-export class DialogoVerificar implements OnDestroy {
+// export class DialogoVerificar implements OnDestroy {
 
-  esconderClave: boolean;
+//   esconderClave: boolean;
 
-  formulario_verificar_clave = new FormGroup({
+//   formulario_verificar_clave = new FormGroup({
 
-    clave: new FormControl('', [Validators.required]),
+//     clave: new FormControl('', [Validators.required]),
 
-  });
+//   });
 
-  usuario: Login = {
-    cuenta: null,
-    password: null,
+//   usuario: Login = {
+//     cuenta: null,
+//     password: null,
 
-  }
+//   }
 
-  administrador: Administrador = {
-    id_administrador: null,
-    usuario: null,
-    password: null,
-    nombre_completo: null,
-    identidad: null
-  };
+//   administrador: Administrador = {
+//     id_administrador: null,
+//     usuario: null,
+//     password: null,
+//     nombre_completo: null,
+//     identidad: null
+//   };
 
-  administradores: Administrador[];
+//   administradores: Administrador[];
 
-  constructor(public dialogRef: MatDialogRef<DialogoVerificar>,
-    private loginAdminService: LoginadminService,
-    private loginService: LoginService,
-    private mensaje: MatSnackBar,
-    private router: Router,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
+//   constructor(public dialogRef: MatDialogRef<DialogoVerificar>,
+//     private loginAdminService: LoginadminService,
+//     private loginService: LoginService,
+//     private mensaje: MatSnackBar,
+//     private router: Router,
+//     @Inject(MAT_DIALOG_DATA) public data: any) {
 
 
-  }
+//   }
 
 
-  //EVENTO CUANDO SE DA ENTER
-  onKeydown(event) {
+//   //EVENTO CUANDO SE DA ENTER
+//   onKeydown(event) {
 
-    if (event.key === "Enter") {
-      this.esconderClave = true;
-      this.verificar();
-    }
+//     if (event.key === "Enter") {
+//       this.esconderClave = true;
+//       this.verificar();
+//     }
 
-  }
+//   }
 
-  salir() {
-    this.dialogRef.close();
-  }
+//   salir() {
+//     this.dialogRef.close();
+//   }
 
-  getAdministradores() {
-    this.loginAdminService.obtenerAdministradores().subscribe((data: any[]) => {
-      this.administradores = data;
-    })
-  }
+//   getAdministradores() {
+//     this.loginAdminService.obtenerAdministradores().subscribe((data: any[]) => {
+//       this.administradores = data;
+//     })
+//   }
 
-  guardar() {
+//   guardar() {
 
-    this.verificar();
+//     this.verificar();
 
-  }
+//   }
 
-  verificar() {
+//   verificar() {
 
 
-    if (this.formulario_verificar_clave.valid) {
+//     if (this.formulario_verificar_clave.valid) {
 
-      this.usuario.cuenta = this.loginService.datosUsuario.cuenta;
-      this.usuario.password = this.clave.value;
+//       this.usuario.cuenta = this.loginService.datosUsuario.cuenta;
+//       this.usuario.password = this.clave.value;
 
-      this.loginService.verificarClave(this.usuario).subscribe((data: any) => {
+//       this.loginService.verificarClave(this.usuario).subscribe((data: any) => {
 
-        if (data.codigoError == 0) {
+//         if (data.codigoError == 0) {
 
-          if (this.data.editando) {
+//           if (this.data.editando) {
 
 
 
-            // this.disabledloginadmin = true;
+//             // this.disabledloginadmin = true;
 
-            // this.administrador.password = this.data.formulario.get('contraseniaC').value;
-            this.administrador.id_administrador = this.data.id;
-            this.administrador.usuario = this.data.formulario.get('usuario').value;
-            // this.administrador.password = this.contraseniaC.value;
-            this.administrador.nombre_completo = this.data.formulario.get('nombre').value;
-            this.administrador.identidad = this.data.formulario.get('identidad').value;
+//             // this.administrador.password = this.data.formulario.get('contraseniaC').value;
+//             this.administrador.id_administrador = this.data.id;
+//             this.administrador.usuario = this.data.formulario.get('usuario').value;
+//             // this.administrador.password = this.contraseniaC.value;
+//             this.administrador.nombre_completo = this.data.formulario.get('nombre').value;
+//             this.administrador.identidad = this.data.formulario.get('identidad').value;
 
-            this.loginAdminService.actualizarAdministrador(this.administrador).subscribe((data) => {
-              console.log(data);
-              this.router.navigate(['/principal/veradministradores']);
-              this.getAdministradores();
-              this.showError('Administrador actualizado correctamente');
+//             this.loginAdminService.actualizarAdministrador(this.administrador).subscribe((data) => {
+//               console.log(data);
+//               this.router.navigate(['/principal/veradministradores']);
+//               this.getAdministradores();
+//               this.showError('Administrador actualizado correctamente');
 
-            }, (error) => {
-              console.log(error);
-              alert('se chorrio');
-            });
+//             }, (error) => {
+//               console.log(error);
+//               alert('se chorrio');
+//             });
 
 
 
-          } else {
+//           } else {
 
-            this.administrador.password = this.data.formulario.get('contraseniaC').value;
+//             this.administrador.password = this.data.formulario.get('contraseniaC').value;
 
-            if (this.administrador.password == this.data.formulario.get('contrasenia').value) {
+//             if (this.administrador.password == this.data.formulario.get('contrasenia').value) {
 
-              this.administrador.usuario = this.data.formulario.get('usuario').value;
-              this.administrador.password = this.data.formulario.get('contrasenia').value;
-              this.administrador.nombre_completo = this.data.formulario.get('nombre').value;
-              this.administrador.identidad = this.data.formulario.get('identidad').value;
+//               this.administrador.usuario = this.data.formulario.get('usuario').value;
+//               this.administrador.password = this.data.formulario.get('contrasenia').value;
+//               this.administrador.nombre_completo = this.data.formulario.get('nombre').value;
+//               this.administrador.identidad = this.data.formulario.get('identidad').value;
 
-              if (this.data.formulario.valid) {
+//               if (this.data.formulario.valid) {
 
-                // this.disabledloginadmin = true;
+//                 // this.disabledloginadmin = true;
 
-                this.loginAdminService.guardarAdministrador(this.administrador).subscribe((data) => {
+//                 this.loginAdminService.guardarAdministrador(this.administrador).subscribe((data) => {
 
-                  this.getAdministradores();
+//                   this.getAdministradores();
 
-                  this.router.navigate(['/principal/veradministradores']);
+//                   this.router.navigate(['/principal/veradministradores']);
 
-                  this.showError('Administrador creado con exito');
+//                   this.showError('Administrador creado con exito');
 
-                }, (error) => {
+//                 }, (error) => {
 
-                  console.log(error);
+//                   console.log(error);
 
-                });
+//                 });
 
-              } else {
+//               } else {
 
-                this.showError('Ingrese los datos correctamente');
-              }
+//                 this.showError('Ingrese los datos correctamente');
+//               }
 
-            } else {
+//             } else {
 
-              this.showError('La contraseña no coincide');
+//               this.showError('La contraseña no coincide');
 
-            }
-          }
+//             }
+//           }
 
-          this.salir();
+//           this.salir();
 
-        } else {
+//         } else {
 
-          this.showError(data.msg);
+//           this.showError(data.msg);
 
-        }
+//         }
 
-      }, (error) => {
+//       }, (error) => {
 
-        console.log(error);
+//         console.log(error);
 
-      });
+//       });
 
 
-    }
+//     }
 
-  }
+//   }
 
 
-  showError(message: string) {
-    const config = new MatSnackBarConfig();
-    config.panelClass = ['background-red'];
-    config.duration = 2000;
-    this.mensaje.open(message, null, config);
-  }
+//   showError(message: string) {
+//     const config = new MatSnackBarConfig();
+//     config.panelClass = ['background-red'];
+//     config.duration = 2000;
+//     this.mensaje.open(message, null, config);
+//   }
 
-  ngOnDestroy(): void { }
+//   ngOnDestroy(): void { }
 
-  get clave() { return this.formulario_verificar_clave.get('clave') };
+//   get clave() { return this.formulario_verificar_clave.get('clave') };
 
-} 
+// } 
