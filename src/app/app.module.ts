c005391319c38106fa70eaa1cf2b455ed577425d
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2020-03-19 09:49:23 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Route } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { PrincipalComponent, DialogContentExampleDialog1, DialogCerrarSesion2 } from './principal/principal.component';
+import { PrincipalComponent,  DialogCerrarSesion2 } from './principal/principal.component';
 import { LoginadminComponent, DialogoCambiarContraseniaAdmin, /*DialogoVerificar*/ } from './loginadmin/loginadmin.component';
 import { NgxPasswordToggleModule } from 'ngx-password-toggle';
 import { At1Component } from './at1/at1.component';
@@ -166,7 +166,6 @@ const routes: Route[] = [
     VerPacienteComponent,
     cambiocontraDialog,
     InventarioComponent,
-    DialogContentExampleDialog1,
     actualizarcontraDialog,
     verificarDialog,
     DialogCerrarSesion,
@@ -259,7 +258,6 @@ const routes: Route[] = [
   ],
   entryComponents: [
     cambiocontraDialog,
-    DialogContentExampleDialog1,
     actualizarcontraDialog,
     verificarDialog,
     DialogCerrarSesion,
