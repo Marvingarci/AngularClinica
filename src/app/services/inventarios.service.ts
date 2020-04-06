7510af0bd6be4e8f598910b3e7afb19c4fad1aac
bagtrejo <braslytrejo@gmail.com>
2020-04-05 18:51:05 -0600

    Update inventarios.service.ts

--- src/app/services/inventarios.service.ts
+++ src/app/services/inventarios.service.ts
@@ -7,10 +7,9 @@ import { HistoriaSubsiguiente } from "../interfaces/historia_subsiguiente";
   providedIn: 'root'
 })
 export class InventariosService {
-  id_historia_subsiguiente: any;
-  imagenactual: string;
+ 
   API_ENDPOINT = 'http://127.0.0.1:8000/api'
-  sihayimagen: boolean = false;
+
   getInventario(id_inventario: any){
     return this.httpClient.get(this.API_ENDPOINT+'/inventarios/'+id_inventario);
   }
@@ -30,29 +29,15 @@ export class InventariosService {
     return this.httpClient.put(this.API_ENDPOINT+'/inventarios/' + inventario.id_inventario, inventario,{headers:headers});
   }
 
-  guardarHistoriaSubsiguiente(historia_subsiguiente: HistoriaSubsiguiente){
-    const headers = new HttpHeaders({'Content-Type':'application/json'});
-    return this.httpClient.post(this.API_ENDPOINT+'/historias_subsiguientes',historia_subsiguiente,{headers:headers});
-  }
   
-  obtenerHistoriaSubsiguiente(id_paciente: any){
-    return this.httpClient.get(this.API_ENDPOINT+'/historias_subsiguientes/'+id_paciente);
-  }
-  // Obtener las citas diarias para consolidado  
-  obtenerTodasCita(){
-    return this.httpClient.get(this.API_ENDPOINT+'/historias_subsiguientes');
-  }
   obtenerMedicamento(){
     return this.httpClient.get(this.API_ENDPOINT+'/medicamentos');
   }
+
   EgresoMedicamentos(medicamento){
     const headers = new HttpHeaders({'Content-Type':'application/json'});
     return this.httpClient.post(this.API_ENDPOINT+'/medicamentos/egreso',medicamento,{headers:headers});
   }
-  ActualizarImagen(datos){
-    
-    const headers = new HttpHeaders({'Content-Type':'application/json'});
-    return this.httpClient.post(this.API_ENDPOINT+'/pacientes/actualizarImagen',datos, {headers:headers});
-  }
+  
 
 }
