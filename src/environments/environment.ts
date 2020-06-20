1cfc7bdf80842414f197fc7696e01cf72805fe56
bagtrejo <braslytrejo@gmail.com>
2020-06-20 10:37:26 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB5SO98-f8y_HCcbvDqVXinyR2OLda1g1M",
+    authDomain: "chatclinicaunahtec-c11ca.firebaseapp.com",
+    databaseURL: "https://chatclinicaunahtec-c11ca.firebaseio.com",
+    projectId: "chatclinicaunahtec-c11ca",
+    storageBucket: "chatclinicaunahtec-c11ca.appspot.com",
+    messagingSenderId: "190073905473",
+    appId: "1:190073905473:web:f8f82f6b6b7b5f3ef4c1c0"
+  }
 };
 
 /*
