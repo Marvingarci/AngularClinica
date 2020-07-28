4a70d324e493ccbe2b606d0e084e99771b9f1174
Wuilmer <wuilmeravila31@gmail.com>
2020-07-28 09:50:20 -0600

    Revert "Merge branch 'master' of https://github.com/Marvingarci/AngularClinica"
    This reverts commit 8848a90eed27ca7c4fb81d6f6b80a21a40eb44c0, reversing
    changes made to 1e3c1fb48a7362decf8024fc8ac726e7b7347405.

--- src/app/validations/correo-unico.directive.ts
+++ src/app/validations/correo-unico.directive.ts
@@ -1,26 +1,33 @@
-import { Directive, Injectable } from '@angular/core';
-import { NG_ASYNC_VALIDATORS, AsyncValidator } from '@angular/forms';
+import { Directive, Injectable} from '@angular/core';
+import { AsyncValidator, NG_ASYNC_VALIDATORS, AbstractControl } from '@angular/forms';
 import { FormularioService } from '../services/formulario.service';
 import { map } from 'rxjs/operators';
 
 @Directive({
   selector: '[correoUnico]',
-  providers: [{  provide: NG_ASYNC_VALIDATORS, useExisting: CorreoUnicoDirective, multi: true }]
+  providers: [{ provide: NG_ASYNC_VALIDATORS, useExisting: CorreoUnicoDirective, multi: true }]
 })
 export class CorreoUnicoDirective implements AsyncValidator {
+  static validate(control: AbstractControl): import("rxjs").Observable<import("@angular/forms").ValidationErrors> | Promise<import("@angular/forms").ValidationErrors> {
+    throw new Error("Method not implemented.");
+  }
 
   constructor(private FormularioService: FormularioService) { }
 
   validate(control: import("@angular/forms").AbstractControl): Promise<import("@angular/forms").ValidationErrors> | import("rxjs").Observable<import("@angular/forms").ValidationErrors> {
+    
     const correo = control.value;
+
     return this.FormularioService.obtenerColumnaCorreo(correo).pipe(
-      map((valor:any)=>{
-        
-        if(valor && valor.correo_electronico == correo){
-        
-          return {correoUnico: true};
-        }else
+      map((valor: any) => {
+
+        if (valor && valor.correo_electronico == correo) {
+          return { correoUnico: true };
+
+        } else
+
           return null;
+          
       })
     );
   }
@@ -30,16 +37,17 @@ export class CorreoUnicoDirective implements AsyncValidator {
 
 }
 
+@Injectable({ providedIn: 'root' })
+export class CorreoUnicoService implements AsyncValidator {
+  constructor(private FormularioService: FormularioService) { }
 
-@Injectable({providedIn: 'root'})
-export class CorreoUnicoService implements AsyncValidator  {
- constructor(private FormularioService: FormularioService) { }
+  validate(control: import("@angular/forms").AbstractControl): Promise<import("@angular/forms").ValidationErrors> | import("rxjs").Observable<import("@angular/forms").ValidationErrors> {
 
- validate(control: import("@angular/forms").AbstractControl): Promise<import("@angular/forms").ValidationErrors> | import("rxjs").Observable<import("@angular/forms").ValidationErrors>
- {
-   const correoUnicoDirective = new CorreoUnicoDirective(this.FormularioService);
-    return correoUnicoDirective.validate(control);
- }
+    const correoUnicoDirective = new CorreoUnicoDirective(this.FormularioService);
+
+    return CorreoUnicoDirective.validate(control);
+  
+  }
+
 }
-
 
