da9e6013ecca7cf2f4428c4e68a525fccfd569bf
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2020-08-11 19:35:52 -0600

    Create telEmergValidacion.ts

---
+++ src/app/interfaces/telEmergValidacion.ts
@@ -0,0 +1,7 @@
+export interface TelEmergValidacion{
+    id_telefono_emergencia: number,
+    telefono_emergencia: string,
+    emergencia_persona: string,
+    created_at? : string;
+    updated_at? : string;
+}
