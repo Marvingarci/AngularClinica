b70bc0a62f88284225488bbfe60d5194cc99d5e0
MelvinSevilla <55675919+MelvinSevilla@users.noreply.github.com>
2020-08-11 19:36:07 -0600

    Create telefonoemergencia.directive.spec.ts

---
+++ src/app/validations/telefonoemergencia.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TelefonoemergenciaDirective } from './telefonoemergencia.directive';
+
+describe('TelefonoemergenciaDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TelefonoemergenciaDirective();
+    expect(directive).toBeTruthy();
+  });
+});
